255c934d7ad0b2f7f164ab727909e9e2d84fd975
josephbjr3 <josephbrinkleyjr21@gmail.com>
2023-02-03 23:53:29 -0500

    finished todo

--- todo.js
+++ todo.js
@@ -1,10 +1,10 @@
 let todoAction = prompt("What would you like to do?")
 let todoList = []
-while (todoAction !== 'quit' && todoAction !== 'q') {
+while (todoAction !== "quit" && todoAction !== "q") {
     if (todoAction === "new") {
-        todoAction = prompt("What would you like to add?")
-        todoList.push(todoAction)
-        console.log(`${todoList.indexOf(todoAction)}: ${todoAction} was added`)
+        newTodo = prompt("What would you like to add?")
+        todoList.push(newTodo)
+        console.log(`${todoList.indexOf(newTodo)}: ${newTodo} was added`)
     } else if (todoAction === "list") {
         console.log("*".repeat(9))
         for (let i = 0; i < todoList.length; i++) {
@@ -12,9 +12,15 @@ while (todoAction !== 'quit' && todoAction !== 'q') {
         }
         console.log("*".repeat(9))
     } else if (todoAction === "delete") {
-        todoAction = parseInt(prompt("Type the index of the item you want to delete."))
-        deletedItem = todoList.splice(todoAction, 1)
-        console.log(`${deletedItem} was deleted`)
+        index = parseInt(prompt("Type the index of the item you want to delete."))
+        while (index === NaN || typeof todoList[index] === "undefined") {
+            index = parseInt(prompt("Invalid input. Type a valid index."))
+        }
+        deletedItem = todoList.splice(index, 1)
+        console.log(`${index} : ${deletedItem} was deleted`)
+    }
+    else {
+        alert("Invalid Input")
     }
     todoAction = prompt("What would you like to do?")
 }

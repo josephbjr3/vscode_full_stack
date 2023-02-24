73e2b13366e4995cca992fd77bea758b5bbbe26f
josephbjr3 <josephbrinkleyjr21@gmail.com>
2023-02-23 21:32:19 -0500

    input event practice added

---
+++ input_event_practice/README.md
@@ -0,0 +1,4 @@
+This updates the h1 heading with the text that you type in the input element.
+
+I did not write the HTML.
+I wrote the javascript.
---
+++ input_event_practice/app.js
@@ -0,0 +1,11 @@
+// update the h1 with the inputted text
+
+const input = document.querySelector("#username")
+const h1 = document.querySelector('h1')
+
+input.addEventListener('input', function(e){
+    h1.innerText = `Welcome, ${input.value}`
+    if (input.value === '') {
+        h1.innerText = "Enter Your Username"
+    }
+})
